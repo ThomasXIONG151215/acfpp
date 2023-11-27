d3c3dd239bcaa404f357ae66f713b766f887f8da
Yuanke <60814001+ThomasXIONG151215@users.noreply.github.com>
2023-11-27 11:14:42 +0800

    Update endgame_app.py

--- endgame_app.py
+++ endgame_app.py
@@ -1,18 +1,20 @@
 import streamlit as st
-from PIL import Image
-import pandas as pd 
-from graphviz import Digraph
 from show_canopy import show_canopy
 from show_forecast import show_forecast
 from show_optimize import show_optimize
 from show_plan import show_plan
+from show_context import show_context
 import plotly.express as px 
 
 st.title('Project 711')
 
 with st.sidebar:
     page = st.radio('选择模块',options = ['研究目标','作物长势','温度分布预测','空调流场预测规划','系统选型平台Demo'])
-if page == '作物长势':
+
+if page == '研究目标':
+    show_context()
+
+elif page == '作物长势':
     show_canopy()
 
 elif page == '温度分布预测':
@@ -20,6 +22,6 @@ elif page == '温度分布预测':
     
 elif page == '空调流场预测规划':
     show_optimize()
-
+     
 elif page == '系统选型平台Demo':
     show_plan()
